a52074457d0dd68ca2c75082c9cd91455279f8ac
hema <hema@hema-desktop>
2012-06-13 19:40:40 -0300

    Adição na GUI: MeusInteresses e cadastrar interesse

---
+++ ProjetoSI/src/main/java/estradasolidaria/ui/client/StateCadastrarInteresse.java
@@ -0,0 +1,54 @@
+package estradasolidaria.ui.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.datepicker.client.DateBox;
+import com.google.gwt.user.client.ui.Button;
+
+public class StateCadastrarInteresse extends Composite {
+	public StateCadastrarInteresse() {
+		
+		AbsolutePanel absolutePanel = new AbsolutePanel();
+		initWidget(absolutePanel);
+		absolutePanel.setSize("308px", "266px");
+		
+		Label lblNewLabel = new Label("Origem:");
+		absolutePanel.add(lblNewLabel, 10, 10);
+		
+		Label lblDestino = new Label("Destino:");
+		absolutePanel.add(lblDestino, 10, 33);
+		
+		Label lblData = new Label("Data:");
+		absolutePanel.add(lblData, 10, 63);
+		
+		Label lblHoraincio = new Label("Hora-Início:");
+		absolutePanel.add(lblHoraincio, 10, 95);
+		
+		Label lblHorafim = new Label("Hora-Fim:");
+		absolutePanel.add(lblHorafim, 10, 133);
+		
+		TextBox textBox = new TextBox();
+		absolutePanel.add(textBox, 103, 10);
+		
+		TextBox textBox_1 = new TextBox();
+		absolutePanel.add(textBox_1, 103, 36);
+		
+		DateBox dateBox = new DateBox();
+		absolutePanel.add(dateBox, 103, 63);
+		
+		TextBox textBox_2 = new TextBox();
+		absolutePanel.add(textBox_2, 103, 95);
+		
+		TextBox textBox_3 = new TextBox();
+		absolutePanel.add(textBox_3, 103, 121);
+		
+		Button btnCadastrar = new Button("Cadastrar");
+		absolutePanel.add(btnCadastrar, 10, 174);
+		
+		Button btnCancelar = new Button("Cancelar");
+		absolutePanel.add(btnCancelar, 106, 174);
+		// TODO Auto-generated constructor stub
+	}
+}
--- ProjetoSI/src/main/java/estradasolidaria/ui/client/StateMeusInteresses.java
+++ ProjetoSI/src/main/java/estradasolidaria/ui/client/StateMeusInteresses.java
@@ -1,8 +1,39 @@
 package estradasolidaria.ui.client;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 
 public class StateMeusInteresses extends Composite {
-
-	
+	private List<String> meusInteresses;
+	public StateMeusInteresses(EstradaSolidariaServiceAsync estradaSolidariaService) {
+		meusInteresses = new LinkedList<String>();
+		
+		AbsolutePanel absolutePanel = new AbsolutePanel();
+		initWidget(absolutePanel);
+		
+		ListBox listBox = new ListBox();
+		absolutePanel.add(listBox, 10, 40);
+		listBox.setSize("390px", "246px");
+		listBox.setVisibleItemCount(5);
+		
+		Button btnCadastrarInteresse = new Button("Cadastrar Interesse");
+		btnCadastrarInteresse.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				
+			}
+		});
+		absolutePanel.add(btnCadastrarInteresse, 10, 9);
+		
+		Button btnDeletarInteresse = new Button("Deletar Interesse");
+		absolutePanel.add(btnDeletarInteresse, 155, 9);
+		
+		
+	}
 }
