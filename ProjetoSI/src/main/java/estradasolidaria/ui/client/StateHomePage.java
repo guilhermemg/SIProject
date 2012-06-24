e7ec1f73fe001babc7b1e7dcfb83417d192ffe07
Guilherme Monteiro Gadelha <guilhermemgadelha@gmail.com>
2012-06-24 17:22:28 -0300

    fazer acao de hyperlink

--- ProjetoSI/src/main/java/estradasolidaria/ui/client/StateHomePage.java
+++ ProjetoSI/src/main/java/estradasolidaria/ui/client/StateHomePage.java
@@ -6,6 +6,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
@@ -20,6 +21,8 @@ import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.event.logical.shared.AttachEvent.Handler;
+import com.google.gwt.event.logical.shared.AttachEvent;
 
 public class StateHomePage extends Composite {
 	
@@ -330,7 +333,13 @@ public class StateHomePage extends Composite {
 		Label lblRepositrioDeDesenvolvimento = new Label("Repositório de desenvolvimento:");
 		absolutePanel_4.add(lblRepositrioDeDesenvolvimento, 437, 37);
 		
-		Hyperlink hprlnkRepositrioDeDesenvolvimento = new Hyperlink("https://github.com/guilhermemg/SIProject", false, "newHistoryToken");
+		Anchor hprlnkRepositrioDeDesenvolvimento = new Anchor("https://github.com/guilhermemg/SIProject", false, "newHistoryToken");
+		hprlnkRepositrioDeDesenvolvimento.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent arg0) {
+				//TODO fazer acao de hyperlink
+			}
+		});
+		
 		absolutePanel_4.add(hprlnkRepositrioDeDesenvolvimento, 664, 37);
 		
 		Label lblAssinaturaDoSistema = new Label("Estrada Solidária foi desenvolvido por Guilherme Gadelha, Hemã Vidal, Ítalo Silva e Leonardo Santos.");
