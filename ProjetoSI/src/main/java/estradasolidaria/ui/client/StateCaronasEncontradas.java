aa0c25ca73d50193ba5b9eca43325e5e697fd7ba
Italo Silva <italogas21@gmail.com>
2012-07-23 16:47:06 -0300

    Sugerir ponto de encontro em StateCaronasEncontradas e funcionando

--- ProjetoSI/src/main/java/estradasolidaria/ui/client/StateCaronasEncontradas.java
+++ ProjetoSI/src/main/java/estradasolidaria/ui/client/StateCaronasEncontradas.java
@@ -10,9 +10,11 @@ import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DecoratedStackPanel;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
@@ -25,10 +27,11 @@ public class StateCaronasEncontradas extends Composite {
 	private EstradaSolidariaServiceAsync estradaSolidariaService;
 	private EstradaSolidaria entryPoint;
 	private Integer idSessao;
-	private Integer idCarona;
 	private Label lblMensagemdeerro;
 	private DialogBox newDialog;
 	private TextBox textBox;
+	private TextBox textBoxSolicitar;
+	private Label lblMensagemDeErroSugerir;
 	
 	private DataGrid<GWTCarona> dataGrid_1;
 	private SelectionModel<GWTCarona> selectionModel;
@@ -44,39 +47,15 @@ public class StateCaronasEncontradas extends Composite {
 		
 		AbsolutePanel absolutePanel = new AbsolutePanel();
 		initWidget(absolutePanel);
-		absolutePanel.setSize("885px", "547px");
+		absolutePanel.setSize("885px", "481px");
 		
 		Label lblCaronasEncontradas = new Label("Caronas Encontradas:");
 		lblCaronasEncontradas.setStyleName("gwt-LabelEstradaSolidaria2");
 		absolutePanel.add(lblCaronasEncontradas, 10, 24);
 		
-		AbsolutePanel absolutePanel_1 = new AbsolutePanel();
-		absolutePanel.add(absolutePanel_1, 80, 64);
-		absolutePanel_1.setSize("797px", "106px");
-		
-		Label lblNewLabel = new Label("Sugira um local de encontro: (Opcional)");
-		lblNewLabel.setStyleName("gwt-LabelEstradaSolidaria8");
-		absolutePanel_1.add(lblNewLabel, 47, 46);
-		
-		textBox = new TextBox();
-		absolutePanel_1.add(textBox, 294, 46);
-		textBox.setSize("260px", "16px");
-		
-		TextButton txtbtnRequisitarVaga = new TextButton("Requisitar ");
-		absolutePanel_1.add(txtbtnRequisitarVaga, 579, 42);
-		
-		lblMensagemdeerro = new Label("MensagemDeErro");
-		lblMensagemdeerro.setStyleName("gwt-LabelEstradaSolidaria5");
-		lblMensagemdeerro.setVisible(false);
-		absolutePanel_1.add(lblMensagemdeerro, 294, 76);
-		
-		Label lblNewLabel_1 = new Label("Requisitar Vaga em carona");
-		lblNewLabel_1.setStyleName("gwt-LabelEstradaSolidaria4");
-		absolutePanel_1.add(lblNewLabel_1, 47, 24);
-		
 		dataGrid_1 = new DataGrid<GWTCarona>();
-		absolutePanel.add(dataGrid_1, 80, 176);
-		dataGrid_1.setSize("797px", "257px");
+		absolutePanel.add(dataGrid_1, 78, 61);
+		dataGrid_1.setSize("797px", "187px");
 		
 		selectionModel = new MultiSelectionModel<GWTCarona>();
 		dataGrid_1.setSelectionModel(selectionModel);
@@ -134,7 +113,73 @@ public class StateCaronasEncontradas extends Composite {
 		
 		dataGrid_1.setRowCount(listaDeCaronas.size(), true);
 		dataGrid_1.setRowData(listaDeCaronas);
-
+		
+		DecoratedStackPanel decoratedStackPanel = new DecoratedStackPanel();
+		absolutePanel.add(decoratedStackPanel, 78, 264);
+		decoratedStackPanel.setSize("387px", "180px");
+		
+		AbsolutePanel absolutePanel_1 = new AbsolutePanel();
+		decoratedStackPanel.add(absolutePanel_1, "Requisitar vaga em carona", false);
+		absolutePanel_1.setSize("100%", "100%");
+		
+		Label lblNewLabel = new Label("Sugira um local de encontro: (Opcional)");
+		lblNewLabel.setStyleName("gwt-LabelEstradaSolidaria8");
+		absolutePanel_1.add(lblNewLabel, 10, 20);
+		
+		textBox = new TextBox();
+		absolutePanel_1.add(textBox, 43, 42);
+		textBox.setSize("325px", "16px");
+		
+		TextButton txtbtnRequisitarVaga = new TextButton("Requisitar ");
+		absolutePanel_1.add(txtbtnRequisitarVaga, 296, 88);
+		txtbtnRequisitarVaga.setSize("80px", "43px");
+		
+		lblMensagemdeerro = new Label("MensagemDeErro");
+		lblMensagemdeerro.setStyleName("gwt-LabelEstradaSolidaria5");
+		lblMensagemdeerro.setVisible(false);
+		absolutePanel_1.add(lblMensagemdeerro, 43, 72);
+		
+		DecoratedStackPanel decoratedStackPanel_1 = new DecoratedStackPanel();
+		absolutePanel.add(decoratedStackPanel_1, 488, 264);
+		decoratedStackPanel_1.setSize("387px", "180px");
+		
+		AbsolutePanel absolutePanel_2 = new AbsolutePanel();
+		decoratedStackPanel_1.add(absolutePanel_2, "Sugerir ponto de encontro", false);
+		absolutePanel_2.setSize("100%", "100%");
+		
+		Label lblLocal = new Label("Local:");
+		absolutePanel_2.add(lblLocal, 10, 20);
+		
+		textBoxSolicitar = new TextBox();
+		absolutePanel_2.add(textBoxSolicitar, 32, 43);
+		textBoxSolicitar.setSize("312px", "16px");
+		
+		lblMensagemDeErroSugerir = new Label("");
+		absolutePanel_2.add(lblMensagemDeErroSugerir, 32, 73);
+		lblMensagemDeErroSugerir.setStyleName("gwt-LabelEstradaSolidaria5");
+		lblMensagemDeErroSugerir.setVisible(false);
+		
+		TextButton txtbtnEnviar = new TextButton("Enviar");
+		txtbtnEnviar.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				GWTCarona selecionada = null;
+				for(GWTCarona c : listaDeCaronas){
+					if(checkColumn.getValue(c)){
+						selecionada = c;
+					}
+				}
+				Integer idCarona = Integer.parseInt(selecionada.getIdCarona());
+				if(textBoxSolicitar.getText().length() == 0){
+					lblMensagemDeErroSugerir.setText("Ponto inválido");
+					lblMensagemDeErroSugerir.setVisible(true);
+				} else {
+					sugerirPontoDeEncontroGUI(textBoxSolicitar.getText(), idCarona);
+				}
+			}
+		});
+		absolutePanel_2.add(txtbtnEnviar, 295, 91);
+		txtbtnEnviar.setSize("57px", "39px");
+		
 		txtbtnRequisitarVaga.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent arg0) {
 				GWTCarona selecionada = null;
@@ -143,7 +188,7 @@ public class StateCaronasEncontradas extends Composite {
 						selecionada = c;
 					}
 				}
-				idCarona = Integer.parseInt(selecionada.getIdCarona());
+				Integer idCarona = Integer.parseInt(selecionada.getIdCarona());
 				newDialog = new DialogBoxNovaSolicitacao(estradaSolidariaService, idCarona, idSessao);
 				newDialog.center();
 				newDialog.hide();
@@ -157,6 +202,23 @@ public class StateCaronasEncontradas extends Composite {
 		});
 	}
 	
+	protected void sugerirPontoDeEncontroGUI(String ponto, Integer idCarona) {
+		estradaSolidariaService.sugerirPontoEncontro(idSessao, idCarona, ponto, new AsyncCallback<Void>(){ 
+			@Override
+			public void onFailure(Throwable caught) {
+				// Show the RPC error message to the user 
+				lblMensagemDeErroSugerir.setText(caught.getMessage());
+				lblMensagemDeErroSugerir.setVisible(true);
+			}
+
+			@Override
+			public void onSuccess(Void result) {
+				Window.alert("Foi!");
+			}
+		  });	
+		
+	}
+
 	protected void solicitarVagaGUI(Integer idSessao, Integer idCarona) {
 		estradaSolidariaService.solicitarVaga(idSessao, idCarona, new AsyncCallback<String>(){ 
 			@Override
